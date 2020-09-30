878df5a130e8a390363cd530a069d96cb52a4cc0
Quentin MOUQUET <quentin.mouquet@isen.yncrea.fr>
2020-09-30 17:19:50 +0200

    Fin de l'exo 4

--- TP3/Exo4/Exo4.c
+++ TP3/Exo4/Exo4.c
@@ -11,7 +11,11 @@ int main() {
 
 	unsigned int Adresse_machine = 0;
 	Adresse_machine = (MyAddress_w << 24) + (MyAddress_x << 16) + (MyAddress_y << 8) + MyAddress_z;
-	printf("\n%u", Adresse_machine);
+	printf("\n%d", MyAddress_w);
+	printf(".%d", MyAddress_x);
+	printf(".%d", MyAddress_y);
+	printf(".%d", MyAddress_z);
+	printf(" /%d\n", IPv4MaskLength);
 
 
 	unsigned int masque = 0;
@@ -20,8 +24,9 @@ int main() {
 		masque = masque + pow(2, 31 - i);
 	}
 
-	unsigned long int Adresse_reseau = 0;
+	unsigned int Adresse_reseau = 0;
 	Adresse_reseau = Adresse_machine & masque;
+	unsigned int tmp = Adresse_reseau;
 
 	printf("\n%d", Adresse_reseau >> 24);
 	Adresse_reseau = Adresse_reseau<<8;
@@ -33,5 +38,22 @@ int main() {
 	Adresse_reseau = Adresse_reseau << 24;
 	Adresse_reseau = Adresse_reseau >> 24;
 	printf(".%d", (Adresse_reseau));
+	printf(" /%d", IPv4MaskLength);
+
+	printf("\n");
+	unsigned int Adresse_broadcast;
+	Adresse_reseau = tmp;
+	Adresse_broadcast = Adresse_reseau | ~(masque);
+
+	printf("\n%d", Adresse_broadcast >> 24);
+	Adresse_broadcast = Adresse_broadcast << 8;
+	Adresse_broadcast = Adresse_broadcast >> 8;
+	printf(".%d", (Adresse_broadcast >> 16));
+	Adresse_broadcast = Adresse_broadcast << 16;
+	Adresse_broadcast = Adresse_broadcast >> 16;
+	printf(".%d", (Adresse_broadcast >> 8));
+	Adresse_broadcast = Adresse_broadcast << 24;
+	Adresse_broadcast = Adresse_broadcast >> 24;
+	printf(".%d", (Adresse_broadcast));
 	printf(" /%d", IPv4MaskLength);
 }
